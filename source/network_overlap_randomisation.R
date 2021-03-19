7484b5ca3eff71d51664ba8b45f76201269729e2
Pisanu Buphamalai <pbuphamalai@cemm.oeaw.ac.at>
2021-03-20 00:36:58 +0100

    network similarity randomisation added, remove archived folders

--- source/network_overlap_randomisation.R
+++ source/network_overlap_randomisation.R
@@ -1,112 +1,101 @@
-## randomised networks
+## network similarity randomisation
 ## IzeBuphamalai, 11.03.2021
 
 ## Goals
-### to get a reference distribution for networks, and their degree of overlap
+### to get a reference distribution of overlaps for all pairs of networks, and compute the significance
 
-# Formulation
-#  For a list of grpah with V and E, from maximal poolcompute pairwise overlap
-
-
-# 0 - set up -------------
-source("../functions/readdata_functions.R")
-library(igraph)
-
-
-# 1 - load required files -----------
-g = process_graph_data("../data/network_edgelists/")
-
-all_nodes = sapply(g, function(x) V(x)$name) %>% unlist %>% unique 
-
-A = matrix()
-
-prob = 0.4
-A = matrix(sapply(1:16, function(x) rbinom(1, size = 1, prob = 0.2)), nrow = 4, dimnames = list(letters[1:4], letters[1:4]))
-B = matrix(sapply(1:16, function(x) rbinom(1, size = 1, prob = 0.2)), nrow = 4, dimnames = list(letters[1:4], letters[1:4]))
-
-B_r = B
-dimnames(B_r)[[1]] = sample(dimnames(B_r)[[1]], nrow(B_r))
-dimnames(B_r)[[2]] = dimnames(B_r)[[1]]
-
-gB_r = graph_from_adjacency_matrix(B_r)
-g_A = graph_from_adjacency_matrix(A)
-g_B = graph_from_adjacency_matrix(B)
-
-
-g_A = g$`co-essential`
-g_B = g$GOBP
-g_C = g$GOMF
-
-
-
-network_sim_df
-###############
-gs <- g
-# 
-# all_nodes <- lapply(gs, function(x) V(x)$name) %>% as_vector() %>% unique 
-# ids = 1:length(all_nodes); names(ids) = all_nodes
-
-# for(i in 1:length(gs)){
-#   V(gs[[i]])$name <-  ids[V(gs[[i]])$name]
-# }
-
-ecounts <- sapply(gs, ecount)
-
-node_index_randomisation <- function(graph){
-  #V(graph)$name <- sample(names(V(graph)), vcount(graph), replace = F)
-  name <- sample(names(V(graph)), vcount(graph), replace = F)
-  #return(graph)
-  return(name)
+if(!file.exists("../cache/overlap_similarity_randomisation_results.RDS")){
   
-}
-
-# maximum number of randomization
-max_rand <- 30
-#g_rand <- lapply(gs, function(graph) lapply(1:max_rand, function(x) node_index_randomisation(graph)))
-name_rand <- lapply(gs, function(graph) lapply(1:max_rand, function(x) node_index_randomisation(graph)))
-
-
-blank_result_mat <- matrix(NA, nrow = max_rand, ncol = max_rand)
-
-
-overlap_randomisation <- function(g, name_gA, name_gB){
-  g_A <- g[[name_gA]]
-  g_B <- g[[name_gB]]
+  print("no precomputed results found. Starting the randomisation. This will take long...")
   
-  union_mat <- blank_result_mat
-  intersect_mat <- blank_result_mat
+  # 0 - set up -------------
+  library(pacman)
   
-  for(i in 1:30){
-    V(g_A)$name <- name_rand[[name_gA]][[i]]
+  p_load(reshape2, pbpply, tidyverse, igraph)
+  
+  source("../functions/readdata_functions.R")
+  
+  # load graphs
+  g = process_graph_data("../data/network_edgelists/")
+  
+  
+  # 1 - set up functions and preliminary variables -----------
+  
+  # a vector of all nodes on all layers
+  all_nodes <- lapply(g, function(x) V(x)$name) %>% unlist %>% unique()
+  
+  # counts edges for each layers
+  ecounts <- sapply(g, ecount)
+  
+  
+  node_index_randomisation <- function(graph){
+    # a function to randomise node ids
+    # input: a graph
+    # output: a randomised node ids
     
-    for(j in 1:30){
-      V(g_B)$name <- name_rand[[name_gB]][[j]]
-      
-      intersect_mat[i,j] <- graph.intersection(g_A, g_B) %>% ecount
-      union_mat[i,j] <- graph.union(g_A, g_B) %>% ecount
-    }
+    name <- sample(names(V(graph)), vcount(graph), replace = F)
+    
+    return(name)
   }
- 
-  return(list(intersect = intersect_mat %>% as.vector, union = union_mat %>% as.vector))
+  
+  # set for number of network iterations (total pairs = max_rand^2)
+  max_rand <- 10
+  
+  # a list storing randmomised node ids, each network with the legth of max_rand
+  name_rand <- lapply(g, function(graph) lapply(1:max_rand, function(x) node_index_randomisation(graph)))
+  
+  
+  # create a global variable for storing blank result for each iteration pair to be fed in the randomisation
+  # matrix is blank and is of nxn, n = max_rand
+  blank_result_mat <- matrix(NA, nrow = max_rand, ncol = max_rand)
+  
+  
+  overlap_randomisation <- function(g, name_gA, name_gB, iter = max_rand){
+    # a function to create a pair of randomised networks, and measure overlap and union between them: this function has internal for-loop, which was intentially made, to avoid multiple loading of redundant data
+    # input: g: original graph list, name_gA/B: name index of the graph list, iter: number of iterations 
+    # output: a list of intersect and union of edges from the randomised networks
+    
+    
+    # create random graphs from indices A and B
+    g_A <- g[[name_gA]]
+    g_B <- g[[name_gB]]
+    
+    # create union and intersect matrices to store results from each computation
+    union_mat <- blank_result_mat
+    intersect_mat <- blank_result_mat
+    
+    # use for loops instead of sequential apply framework to save memories and data loading: e
+    for(i in 1:iter){
+      V(g_A)$name <- name_rand[[name_gA]][[i]]
+      
+      for(j in 1:iter){
+        V(g_B)$name <- name_rand[[name_gB]][[j]]
+        
+        # compute intersect and union of graphs
+        intersect_mat[i,j] <- graph.intersection(g_A, g_B) %>% ecount
+        union_mat[i,j] <- graph.union(g_A, g_B) %>% ecount
+      }
+    }
+    
+    return(list(intersect = intersect_mat %>% as.vector, union = union_mat %>% as.vector))
+  }
+  
+  
+  
+  # 2 - perform computation -----------
+  network_sim_df <- readRDS("../cache/network_jaccard_overlap_similarity_df.RDS") %>% dplyr::filter(!grepl("core", V1), !grepl("core" , V2))
+  
+  
+  randomisation_overlap_result <- pbapply(network_sim_df, 1, function(x) overlap_randomisation(g, x[1], x[2])) 
+  
+  # save the result in RDS format and to cache
+  saveRDS(randomisation_overlap_result, "../cache/overlap_similarity_randomisation_results.RDS")
+  
+} else{
+  
+  print("load the precomputed value from cache")
+  
+  randomisation_overlap_result <- readRDS("../cache/overlap_similarity_randomisation_results.RDS")
+  
 }
 
-
-  
-# graph_overlap <- function(g_A, g_B){
-#   intersect <- apply(index_mat, 1, function(x) ) 
-#   union  <- apply(index_mat, 1, function(x) graph.union(g_rand[[g_A]][[x[1]]], g_rand[[g_B]][[x[2]]]) %>% ecount ) 
-#   return(list(intersect, union))
-# }
-
-
-pacman::p_load(pbapply)
-network_sim_df$min_ecount <- pbapply(network_sim_df, 1, function(x) min(ecounts[x[1]], ecounts[x[2]]))
-
-
-# this is the index matrix used for internally computing 
-#ndex_mat <- tidyr::crossing(i = 1:max_rand, j = 1:max_rand)
-
-randomisation_overlap_result <- pbapply(network_sim_df, 1, function(x) overlap_randomisation(g, x[1], x[2])) 
-
-jaccard_index <- lapply(randomisation_overlap, function(x) x[[1]]/x[[2]])
-overlap_index <- lapply(1:length(randomisation_overlap), function(x) randomisation_overlap[[x]][[1]]/graph_combn_df$min_ecount[x])
