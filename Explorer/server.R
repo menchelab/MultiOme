5d5f8668aabd51640a540a062bd579dd09f67757
Pisanu Buphamalai <pbuphamalai@cemm.oeaw.ac.at>
2021-04-30 15:34:19 +0200

    updated Explorer

---
+++ Explorer/server.R
@@ -0,0 +1,122 @@
+
+# load required packages
+library(pacman)
+p_load(ggplot2, visNetwork, igraph, RColorBrewer, shiny, cowplot, DT)
+
+# load required data
+
+
+
+load("./data/precomputedData_server.RData")
+
+# Define server logic 
+shinyServer(function(input, output) {
+  
+  
+  values <- reactiveValues()
+  #############
+  ## Tab 1 ##
+  #############
+  
+  #network plot
+  output$networkplot <- renderVisNetwork({
+
+
+    disease_of_interest <- input$disease
+    graph_of_interest <- network_info$network[network_info$subtype==input$network]
+
+    disease_graph <- subgraphs[[disease_of_interest]][[graph_of_interest]]
+     
+    nodes_df <- tibble(id = V(disease_graph)$name, label = V(disease_graph)$name)
+    edges_df <- igraph::as_data_frame(disease_graph)
+
+    visNetwork(nodes = nodes_df, edges = edges_df, width = "200%") %>%
+      visPhysics(stabilization=F) %>%
+      visEdges(smooth=F, color="grey", width=0.3)  %>%
+      visNodes(color = list(background="white", highlight="magenta", border="black")) %>%
+      visIgraphLayout() %>%
+      visOptions(nodesIdSelection = list(enabled=T, useLabels=T,
+                                         style = 'width: 200px; height: 26px;
+                                         background: #f8f8f8;
+                                         color: black;
+                                         border:none;
+                                         outline:none;'),
+                 highlightNearest = list(enabled =TRUE, degree = 1, hover = T))%>%
+      visInteraction(multiselect = TRUE) %>%
+      #    visGroups(groupname = "upSIG", color = "red") %>%
+      #  visGroups(groupname = "downSIG", color = "blue") %>%
+      visExport(type = "png", name = "Network",
+                float = "left", label = "Save network (png)", background = "white", style= "") %>%
+      visEvents(select = "function(nodes) {
+            Shiny.onInputChange('current_node_id', nodes.nodes);
+            ;}")
+
+
+
+  })
+  
+  
+  
+  output$selectednodes <- renderTable({
+ # tibble(nodes = input$networkplot_selected)
+    tibble(nodes = input$current_node_id)
+  })
+
+  
+  
+  # image2 sends pre-rendered images
+  output$DiffmodImage <- renderImage({
+    if (is.null(input$diseases_DifMod))
+      return(NULL)
+    
+    dis <- input$diseases_DifMod
+    
+    return(list(
+      src = paste0("Figs/disease_modularity/networks_", dis ,"_separate_dimmed.png"),
+      contentType = "image/png",
+      width = 800,
+      alt =  dis
+    ))
+  }, deleteFile = FALSE)
+  
+  
+  
+  # tSNE plot landscape
+  output$LandscapeImage <- renderImage({
+    if (is.null(input$Network_landscape))
+      return(NULL)
+    
+    net <- input$Network_landscape
+    netid <- network_info$network[network_info$subtype==net]
+    
+    return(list(
+      src = paste0("Figs/landscape/", netid, ".png"),
+      contentType = "image/png",
+      width = 800,
+      alt =  net
+    ))
+  }, deleteFile = FALSE)
+  
+
+  # LCC table
+  output$LCCtable <- renderTable({
+    processed_result_df %>%
+      dplyr::filter(name == input$diseases_DifMod, LCC.signif != "none") %>%
+      arrange(desc(LCC.zscore)) %>% 
+      select(subtype, source, N_in_graph, LCC.size, LCC.mean, LCC.sd, LCC.zscore, LCC.signif)
+  })
+  
+  output$LCCbarplot <- renderGirafe({
+    girafe(ggobj = processed_result_df %>%
+      dplyr::filter(name == input$diseases_DifMod, LCC.signif != "none") %>%
+      arrange(desc(LCC.zscore)) %>% 
+      ggplot(., aes(y=subtype, x=LCC.zscore, tooltip = paste0("p-value: ",correctedPval), data_id = subtype)) + 
+        geom_col_interactive() + 
+      facet_grid(main_type~., scales = "free", space = "free") +
+      ylab("Network") + xlab("LCC z-score") + 
+      theme_cowplot() 
+    )
+    })
+  
+  
+})
---
+++ Explorer/ui.R
@@ -0,0 +1,105 @@
+library(pacman)
+p_load(shiny, visNetwork, ggiraph)
+
+load("./data/precomputedData_ui.RData")
+
+select <- dplyr::select
+filter <- dplyr::filter
+
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Rare Disease MultiOme explorer"),
+  
+  # Main panel has 4 tab panels
+  mainPanel(
+    tabsetPanel(
+      #############
+      ## Panel 1 ##
+      #############
+      tabPanel('Disease Differential Modularity',
+               fluidRow(
+                 column(width=3,
+                        selectInput('diseases_DifMod', 'Select rare disease group', 
+                                    diseases_below_1000,
+                                    selected = "Ciliopathy"),
+                        
+                        radioButtons("pType", "Information to show:",
+                                     c("Network" = "network",
+                                       "Table" = "table",
+                                       "Barplot" = "barplot"
+                                     )),
+                        
+
+                 ), #column 1: plot options
+                 column(width=8,
+                         tags$p('Differential modularity of the selected disease group showing underlying causal mechanisms played by gene connectivity on different molecular organizations'),
+                         tags$p('Click dropdown button on the left to change disease groups, and the radio button to change the information shown (Network, Table, or Bar chart)'),
+                        
+
+                        fluidRow(
+                          conditionalPanel('input.pType=="network"', imageOutput("DiffmodImage")),
+                          conditionalPanel('input.pType=="table"', tableOutput("LCCtable")),
+                          conditionalPanel('input.pType=="barplot"', girafeOutput("LCCbarplot"))
+                        )
+                        # plotOutput
+                 )
+               ))
+      ,
+      #######
+      # Tab 2
+      ########
+      tabPanel('Network Landscape',
+               fluidRow(
+                 column(width=3,
+                        selectInput('Network_landscape', 'Select network', 
+                                    network_name,
+                                    selected = "Human Phenotype"))
+                        
+                 ), #column 1: plot options
+                 column(width=8,
+                         tags$p('Network-disease landscape (node2vec + t-SNE) shown for a particular embedded network. THe white dots represent causal genes for the corresponding disease group'),
+                        
+                        
+                        fluidRow(
+                          imageOutput("LandscapeImage")
+                        )
+                        # plotOutput
+                 )
+               )
+      ,
+      ##########
+      # Tab 3
+      ##########
+      tabPanel('Network-disease inspection', 
+               fluidRow(
+                 column(width=3, 
+                        selectInput('disease', 'Select rare disease group', diseases, selected = "Ciliopathy"),
+                        selectInput('network', 'Select network to display', network_name, selected = "Human Phenotype"),
+
+                        
+                 ), # /Column 1: plot options
+                 column(width=8,
+                        tags$p("Inspect gene connectivity for a particular disease on the selected network."),
+                       visNetworkOutput("networkplot", width = "100%"),
+                 tags$p('Zoom in a particular area by scrolling. Gene labels will appear and can be selected upon click.'),
+                 tags$p('Select multiple genes by clicking and holding Cmd (Mac) or Ctrl (Windows/Linux). Selected genes can be enriched via:'),
+                 tags$a(href="https://maayanlab.cloud/Enrichr/", "Enrichr"),
+                 ),#, # /column 2: plot 
+                 column(width=1,
+                        tags$p('Selected genes:'),
+                       # DTOutput("selectednodes", width = "15%")
+                        tableOutput("selectednodes"),
+                       
+                   )# /column 3: download buttons
+                 
+               ) 
+               
+      ) # end tabPanel 3
+    
+
+      
+    ) # /tabsetPanel
+  ) # /mainPanel
+))
