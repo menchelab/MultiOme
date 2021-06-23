00082b532c10131b81fcd099da890fe9292c801c
pisanu.buphamalai <pisanu.buphamalai@clip-c1-20.cbe.vbc.ac.at>
2021-06-23 14:38:37 +0200

    update the reactome network construction script - 10x faster

--- source/build_reactome_network.R
+++ source/build_reactome_network.R
@@ -3,9 +3,10 @@
 ## Ize Buphamalai
 ## Modified June 2021
 
-
 library("igraph")
-
+library(tidyverse)
+library(parallel)
+pacman::p_load(pbapply)
 ####  read pathway gene set from Reactome --------
 reactome_gmt = scan("../data/raw_data/ReactomePathways.gmt", what = "", sep = "\n")
 gmt_to_list = function(x){
@@ -29,6 +30,46 @@ reactome = reactome[!sapply(reactome, length)==1]
 # remove pathways with over 300 genes (uninformative)
 reactome = reactome[!sapply(reactome, length)>300]
 
+
+
+######### build reactome network
+d1 <- stack(reactome)
+
+# take valid gene symbols
+source("../functions/fn_source.R")
+entrez_names <- IDconvert(unique(d1$values), from = "SYMBOL", to = "ENTREZID")
+
+d1 <- d1 %>% filter(values %in% names(entrez_names)[!is.na(entrez_names)])
+
+# create edge lists from d1
+g_gene_pathway <- graph_from_data_frame(d1, directed = F)
+adj_gene_pathway <- get.adjacency(g_gene_pathway, type = "both")
+
+adj_gene_gene <- adj_gene_pathway %*% t(adj_gene_pathway)
+
+gene_element <- colnames(adj_gene_gene) %in% unique(d1$values)
+
+adj_gene_gene <- adj_gene_gene[gene_element, gene_element]
+
+diag(adj_gene_gene) <- NA
+
+gene_gene_df <-get.data.frame(graph_from_adjacency_matrix(adj_gene_gene, weighted = T, diag = F))
+
+# store gene and pathway as id
+gene_id <- unique(d1$values)
+pathway_id <- unique(d1$ind)
+
+# convert data into numeric
+#d1$values <- factor(d1$values, levels = gene_id, labels = 1:length(gene_id) )
+d1$ind <- factor(d1$ind, levels = pathway_id, labels = 1:length(pathway_id) )
+
+reactome_by_element <- split(as.integer(d1$ind), d1$values)
+
+shared_pathway <- pbapply::pbapply(gene_gene_df, 1, function(x) intersect(reactome_by_element[[x[1]]], reactome_by_element[[x[2]]]))#,
+                                 #  cl = cl)
+
+gene_gene_df$shared_pathways <- shared_pathway
+
 # convert into df
 reactome_member_df <- tibble(pathway = names(reactome), member = reactome) %>% 
   unnest(member) 
@@ -41,63 +82,6 @@ reactome_count_df <- reactome_member_df%>%
 ####  save reactome pathway counts -------
 write_tsv(reactome_count_df, "../cache/reactome_pathway_counts.tsv")  
 write_tsv(reactome_member_df, "../cache/reactome_pathway_association.tsv")  
-
-
-
-#### converting into networks --------
-
-# make pair combn of genes in the same pathways
-reactome_list_df = lapply(reactome, function(x) as_tibble(t(combn(x,2, simplify = T))))
-names(reactome_list_df) = names(reactome)
-
-# combine them into one
-reactome_df = bind_rows(reactome_list_df)
-
-
-# count each instance
-reactome_copathway_df = reactome_df %>% 
-  group_by(V1, V2) 
-
-  colnames(reactome_copathway_df) = c("gene1", "gene2", "weight")
-
-## remove the pair with less than four pathways
-#reactome_copathway_filtered_df = reactome_copathway_df[reactome_copathway_df$weight > 1,]
-
-reactome_copathway_filtered_df = reactome_copathway_df
-
-
-
-# invert list:
-d1 <- stack(reactome)
-reactome_by_element <- split(as.character(d1$ind), d1$values)
-
-# take valid gene symbols
-source("../functions/fn_source.R")
-entrez_names <- IDconvert(names(reactome_by_element), from = "SYMBOL", to = "ENTREZID")
-
-# consider only valid genes
-reactome_by_element <- reactome_by_element[names(entrez_names)[!is.na(entrez_names)]]
-
-reactome_comember_df <- as_tibble(t(combn(names(reactome_by_element),2, simplify = T)))
-
-reactome_comember_df$shared_pathway <- apply(reactome_comember_df, 1, function(x) intersect(reactome_by_element[[x[1]]], reactome_by_element[[x[2]]]) )
-
-# apply disparity filter (optional scenario)
-#source("~/Documents/projects/universal_fn/network_functions.R")
-
-#weighted adj mat from all data
-#g_reactome_full = graph_from_data_frame(reactome_copathway_filtered_df, directed = F)
-
-#g_reactome_full = graph_from_data_frame(reactome_copathway_filtered_df, directed = F)
-#adj_reactome_full = as_adjacency_matrix(g_reactome_full, type = "both", attr = "weight")
-#disp_filter = backbone_cal(adj_reactome_full)
-
-#adj_filter = disp_filter < 0.05
-#g_reactome_disp_filter = graph_from_adjacency_matrix(adj_filter, mode = "undirected")
-#el_greactome_disp_filter = as_edgelist(g_reactome_disp_filter) %>% as_tibble() 
-
-#write_tsv(el_greactome_disp_filter, "./network_edgelists/reactome_copathway_disp_filter.tsv")
-
 
 
 # test_multiple scenarios of reactome graph build
